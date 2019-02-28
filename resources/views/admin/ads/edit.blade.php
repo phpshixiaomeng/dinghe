6e9e95b7f8b0aed0d015613687a83947318bfcfe
928787773 <928787773@qq.com>
2019-02-28 15:52:19 +0800

    威少第三次上传

---
+++ resources/views/admin/ads/edit.blade.php
@@ -0,0 +1,111 @@
+@extends('admin/public/public')
+@section('function')
+    $(function() {
+        //自适应屏幕宽度
+        window.onresize = function() { location = location };
+
+        var main_h = $(window).height();
+        $('.hy_list').css('height', main_h - 45 + 'px');
+
+        var main_w = $(window).width();
+        $('.xjhy').css('width', main_w - 40 + 'px');
+
+
+    });
+@endsection
+@section('content')
+
+    <div id="right_ctn">
+        <div class="right_m">
+            <!--会议列表-->
+            <div class="hy_list">
+                <div class="box_t">
+                    <span class="name">修改广告</span>
+                    <!--当前位置-->
+                    <div class="position">
+                        <a href=""><img src="/admin_assets/images/icon5.png" alt=""/></a>
+                        <a href="">首页</a>
+                        <span><img src="/admin_assets/images/icon3.png" alt=""/></span>
+                        <a href="">修改广告</a>
+                    </div>
+                    <!--当前位置-->
+                </div>
+                <form id="images">
+                    {{csrf_field()}}
+                    <input  id="img" name="image" style="display: none;" type="file" onchange="upload()">
+                </form>
+                <div class="space_hx">&nbsp;</div>
+                <!--终端列表-->
+                <form action="/admin/ads/{{ $data->id }}" method="post" enctype="multipart/form-data">
+                    <div class="xjhy">
+                        @if (count($errors) > 0)
+                            <div id="alert" class="alert alert-danger alert-dismissible" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span id="button" aria-hidden="true">&times;</span></button>
+                                @foreach ($errors->all() as $error)
+                                    <strong>{{ $error }}</strong><br>
+                                @endforeach
+                            </div>
+                        @endif
+                        @if (session('error'))
+                            <div id="alert" class="alert alert-danger alert-dismissible" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span id="button" aria-hidden="true">&times;</span></button>
+                                {{ session('error') }}
+                            </div>
+                        @endif
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}
+                        <ul class="hypz">
+                            <li class="clearfix">
+                                <span class="title">游戏名字：</span>
+                                <div class="li_r">
+                                    <input name="gname" value="{{ $data->gname }}" type="text">
+                                <div id="url"></div>
+                                </div>
+                            </li>
+                            <li class="clearfix">
+                                <span class="title">广告标题：</span>
+                                <div class="li_r">
+                                    <input name="title" value="{{ $data->title }}" type="text">
+                                </div>
+                            </li>
+                            <input id="img_thumb" type="hidden" name="image" value="{{ $data->image }}">
+                            <label style="margin-left:150px;margin-top:20px;" for="img" ><img id="thumb" style="width:100px;height:100px;" src="/uploads/{{ $data->image }}" alt=""></label>
+                            <li class="tj_btn">
+                                <input class="btn btn-info" style="margin-left:200px;" type="submit" value="提交">
+                            </li>
+                        </ul>
+                        <!--基本配置-->
+                    </div>
+                </form>
+                <!--终端列表-->
+            </div>
+            <!--会议列表-->
+        </div>
+    </div>
+<script type="text/javascript">
+    function upload()
+    {
+        $.ajax({
+            url: '/admin/ads/upload',
+            type: 'POST',
+            data: new FormData($('#images')[0]),
+            processData: false,
+            contentType: false,
+            dataType:"json",
+            success : function(data) {
+                if(data.msg == 'success'){
+                    $('#thumb').attr('src','/uploads/'+data.path);
+                    $('#img_thumb').val(data.path);
+                }else if(data.msg == 'error'){
+                    alert('图片格式有误,请勿上传同一个文件');
+                }else{
+                    alert('图片大小有误,请勿上传同一个文件');
+                }
+            }
+        });
+    }
+    $('#button').click(function(){
+        $("#alert").hide();
+    });
+</script>
+@endsection
--- resources/views/admin/ads/index.blade.php
+++ resources/views/admin/ads/index.blade.php
@@ -17,12 +17,12 @@
     <div class="right_m">
          <div class="hy_list">
             <div class="box_t">
-                <span class="name">友情链接</span>
+                <span class="name">广告</span>
                     <div class="position">
                         <a href=""><img src="/admin_assets/images/icon5.png" alt=""/></a>
                         <a href="">首页</a>
                         <span><img src="/admin_assets/images/icon3.png" alt=""/></span>
-                        <a href="">友情链接列表</a>
+                        <a href="">广告列表</a>
                     </div>
             </div>
             <script>
@@ -31,16 +31,16 @@
                   ,form = layui.form;
                 });
             </script>
-            <form  action="/admin/link" method="get">
+            <form  action="/admin/ads" method="get">
                 <div class="search">
-                    <span >按照名字查询</span>
+                    <span >按照游戏名字查询</span>
                     <div class="s_text"><input name="search" type="text"></div>
                     <input class="btn btn-primary" type="submit"  value="搜索">
-                    <span style="float:right; font:18px/1.2 '微软雅黑';margin-right: 100px;margin-top: 20px;" >总共<b style="color:red;" >{{ $num }}</b>条友情链接</span>
+                    <span style="float:right; font:18px/1.2 '微软雅黑';margin-right: 100px;margin-top: 20px;" >总共<b style="color:red;" >{{ $num }}</b>条广告</span>
                 </div>
             </form>
             <div class="space_hx">&nbsp;</div>
-             @if (session('success'))
+            @if (session('success'))
                 <div id="alert" class="alert alert-success alert-dismissible" role="alert">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span id="button" aria-hidden="true">&times;</span></button>
                     <strong>{{ session('success') }}</strong>
@@ -52,12 +52,12 @@
                     <strong>{{ session('error') }}</strong>
                 </div>
             @endif
-            <table  class="search" cellpadding="0" cellspacing="0" class="list_hy">
+            <table class="search"  cellpadding="0" cellspacing="0" class="list_hy">
                 <tr>
                     <th class="xz" scope="col">选择</th>
-                    <th scope="col">友情链接名</th>
-                    <th scope="col">友情链接地址</th>
-                    <th scope="col">友情链接缩略图</th>
+                    <th scope="col">游戏名</th>
+                    <th scope="col">广告标题</th>
+                    <th scope="col">广告缩略图</th>
                     <th scope="col">审核</th>
                     <th scope="col">操作</th>
                 </tr>
@@ -65,26 +65,31 @@
                 @foreach($data as $key=>$val)
                 <tr>
                     <td class="xz"><input name="id" type="checkbox" value="{{ $val->id }}"></td>
-                    <td>{{ $val->lname }}</td>
-                    <td>{{ $val->url }}</td>
+                    <td>{{ $val->gname }}</td>
+                    <td>{{ $val->title }}</td>
                     <td>
-                    	<img id="image" onclick="show({{ $val->id }})" src="/uploads/{{ $val->image }}" style="width:40px;height:40px;" alt="">
+                    	<img onclick="show({{ $val->id }})" src="/uploads/{{ $val->image }}" style="width:40px;height:40px;" alt="">
                     </td>
-                    <td>
-                        @if( $val->status == 0 )
-                        	<a href="/admin/link/status/{{ $val->id }}/1"><img style="width:20px" src="/admin_assets/images/links/dui.jpg"></a>
+                    <td style="width:300px">
+                        @if( $val->put == 0 )
+                        <a style="float:left" href="/admin/ads/put/{{ $val->id }}" class="btn btn-info">投放广告</a>
+                        @elseif($val->put ==1)
+                        <a style="float:left" href="/admin/ads/top/{{ $val->id }}" class="btn btn-success">置顶投放</a>
+                        <a style="float:left margin-left" href="/admin/ads/put/{{ $val->id }}" class="btn btn-warning">取消投放</a>
+                        <span style="font:20px/1.6 '微软雅黑'; float:right; font-weight: bold; color:red; margin-right:50px;">已投放</span>
                         @else
-                            <a href="/admin/link/status/{{ $val->id }}/0"><img style="width:20px" src="/admin_assets/images/links/cuo.jpg"></a>
+                        <a style="float:left" href="/admin/ads/top/{{ $val->id }}" class="btn btn-danger">取消置顶</a>
+                        <a style="float:left margin-left" href="/admin/ads/put/{{ $val->id }}" class="btn btn-primary">取消投放</a>
+                        <span style="font:20px/1.6 '微软雅黑'; float:right; font-weight: bold; color:red; margin-right:50px;" >已置顶</span>
                         @endif
                     </td>
                     <td>
-                        <a style="float:left" href="/admin/link/{{ $val->id }}/edit" class="btn btn-warning">修改</a>
-                        <form style="float:left" action="/admin/link/{{ $val->id }}" method="post">
+                        <a style="float:left" href="/admin/ads/{{ $val->id }}/edit" class="btn btn-warning">修改</a>
+                    	<form style="float:left" action="/admin/ads/{{ $val->id }}" method="post">
                             {{ csrf_field() }}
                             {{ method_field('DELETE') }}
                             <input  onclick="return confirm('确定要删除吗?')" class="btn btn-danger" type="submit" value="删除">
                         </form>
-
                     </td>
                 </tr>
                 @endforeach
@@ -93,15 +98,15 @@
                         {{ $data->appends($request)->links() }}
                     </div>
                 <div class="r_foot" style="margin-bottom: 30px">
-                    <div class="r_foot_m">
-                         <span style="float:left;">
+                    <div class="r_foot_m" >
+                        <span style="float:left;">
                             <input id="qx" name="" type="checkbox" value="">
                             <em>全部选中</em>
                         </span>
-                         <span style="width:100px;float:left;margin-top:5px;">
-                            <form method="post" id="qdel" action="/admin/link/del">
+                        <span style="width:100px;float:left;margin-top:5px;">
+                            <form method="post" id="qdel" action="/admin/ads/del">
                                 {{ csrf_field() }}
-                                <input  class="btn btn-danger" onclick="return confirm('确定要删除吗?')"  style="width:100px;height:30px;color:grey;"  type="submit" value="批量删除">
+                                <input class="btn btn-danger"  onclick="return confirm('确定要删除吗?')"  style="width:100px;height:30px;color:grey;" type="submit" value="批量删除">
                             </form>
                         </span>
                     </div>
@@ -136,10 +141,8 @@
           area: ['420px','420px'],
           skin: 'layui-layer-nobg',
           shadeClose: true,
-          content: '/admin/link/'+id
-
+          content: '/admin/ads/'+id
         });
     }
-
 </script>
 @endsection
